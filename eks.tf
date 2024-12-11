7eb93ff6e3ebea637af7f50cf4e8c35c7212141f
paddyzong <youkobak@gmail.com>
2024-12-11 13:50:25 +1100

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,9 +1,8 @@
-# Provider configuration for Kubernetes
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.my_eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.my_eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.my_eks_cluster_auth.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.my_eks_cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.my_eks_cluster.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.my_eks_cluster_auth.token
+# }
 
 # IAM Role for EKS service
 resource "aws_iam_role" "eks_service_role" {
